9e3fd34be07fadbbd39bdd4de248f937c46413df
Wtanga <pkkk.2011@mail.ru>
2019-06-16 16:00:17 +0700

    Adding a loading screen and checking for selections

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 var Timer;
 var steps = 0;
 
+
 function createGrid() {
-  $('.menu').css('transform', 'translateY(-800px)');
-  Timer = setTimeout("loading()", 1000);
+  var teams = document.getElementById("teams");
+  var modes = document.getElementById("mode");
+  var checkSelectedT = teams.options[teams.selectedIndex].value;
+  var checkSelectedM = modes.options[modes.selectedIndex].value;
+  if (checkSelectedT == "0") {
+    $('.errorTeams').css('opacity', '1');
+    document.getElementById("errorT").innerHTML = "Выберите количество команд";
+  } else if (checkSelectedM == "0") {
+    $('.errorMode').css('opacity', '1');
+    document.getElementById("errorM").innerHTML = "Выберите режим турнира";
+  } else {
+    $('.menu').css('transform', 'translateY(-800px)');
+    Timer = setTimeout("loading()", 1000);
+  }
 }
 
 function loading() {
@@ -28,10 +41,12 @@ function loader() {
 
 function getTeams(selectObject) {
   var value = selectObject.value;
+  $('.errorTeams').css('opacity', '0');
   console.log(value);
 }
 
 function getMode(selectObject) {
   var value = selectObject.value;
+  $('.errorMode').css('opacity', '0');
   console.log(value);
 }
