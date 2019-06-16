78ffa26fa22dd265ff34ef95b165bf2535ead54a
Wtanga <pkkk.2011@mail.ru>
2019-06-16 16:09:47 +0700

    Sketch of the tournament grid layout

--- js/main.js
+++ js/main.js
@@ -1,22 +1,111 @@
 var Timer;
 var steps = 0;
-
+var stepsTimer = 0;
+var valueT;
+var valueM;
+var divGrid = '<div class="gridSquads" id="grid-$id"></div>';
 
 function createGrid() {
-  var teams = document.getElementById("teams");
-  var modes = document.getElementById("mode");
+  var teams = id("teams");
+  var modes = id("mode");
   var checkSelectedT = teams.options[teams.selectedIndex].value;
   var checkSelectedM = modes.options[modes.selectedIndex].value;
   if (checkSelectedT == "0") {
     $('.errorTeams').css('opacity', '1');
-    document.getElementById("errorT").innerHTML = "Выберите количество команд";
+    id("errorT").innerHTML = "Выберите количество команд";
   } else if (checkSelectedM == "0") {
     $('.errorMode').css('opacity', '1');
-    document.getElementById("errorM").innerHTML = "Выберите режим турнира";
+    id("errorM").innerHTML = "Выберите режим турнира";
   } else {
     $('.menu').css('transform', 'translateY(-800px)');
     Timer = setTimeout("loading()", 1000);
   }
+}
+
+function displayGrid() {
+  $('.leftOne').html('');
+  $('.rightOne').html('');
+  $('.leftTwo').html('');
+  $('.rightTwo').html('');
+
+  var half = valueT / 2;
+  var halfD = half / 2;
+  var halfT = halfD / 2;
+  var halfF = halfT / 2;
+  var halfFi = halfF / 2;
+
+  for (var i = 0; i < valueT; i++) {
+    if (i + 1 <= half) {
+      $('.leftOne').append(divGrid
+        .replace('$id', +i + 1)
+        .replace('gridSquads', 'gridSquads gridLeftOne'));
+      id('grid-' + (i + 1)).innerHTML = "Team #" + (i + 1);
+      if ((i + 1) % 2 == 1) {
+        $('.linear').css({
+          'display': 'block',
+          'width': '65px',
+          'height': '65px',
+          'border': '1px solid lightgray',
+          'position': 'absolute'
+        });
+      }
+    } else {
+      $('.rightOne').append(divGrid
+        .replace('$id', +i + 1)
+        .replace('gridSquads', 'gridSquads gridRightOne'));
+      id('grid-' + (i + 1)).innerHTML = "Team #" + (i + 1);
+    }
+  }
+
+  for (var i = 0; i < valueT / 2; i++) {
+    if (i + 1 <= halfD) {
+      $('.leftTwo').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridLeftTwo'));
+    } else {
+      $('.rightTwo').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridRightTwo'));
+    }
+  }
+
+  for (var i = 0; i < valueT / 4; i++) {
+    if (i + 1 <= halfT) {
+      $('.leftThree').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridLeftThree'));
+    } else {
+      $('.rightThree').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridRightThree'));
+    }
+  }
+
+  for (var i = 0; i < valueT / 8; i++) {
+    if (i + 1 <= halfF) {
+      $('.leftFour').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridLeftFour'));
+    } else {
+      $('.rightFour').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridRightFour'));
+    }
+  }
+
+  for (var i = 0; i < valueT / 16; i++) {
+    if (i + 1 <= halfFi) {
+      $('.leftFive').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridLeftFive'));
+    } else {
+      $('.rightFive').append(divGrid
+        .replace('$id', 'empty')
+        .replace('gridSquads', 'gridSquads gridRightFive'));
+    }
+  }
+
+  $('.grid').css('display', 'block');
 }
 
 function loading() {
@@ -26,27 +115,39 @@ function loading() {
 }
 
 function loader() {
-  console.log(steps);
   steps++;
+  stepsTimer++;
+  console.log(stepsTimer);
   if (steps == 1) {
-    document.getElementById("load").innerHTML = ".";
+    id("load").innerHTML = ".";
   } else if (steps == 2) {
-    document.getElementById("load").innerHTML = "..";
+    id("load").innerHTML = "..";
   } else if (steps == 3) {
-    document.getElementById("load").innerHTML = "...";
+    id("load").innerHTML = "...";
     steps = 0;
   }
   Timer = setTimeout("loader()", 1000);
+  if (stepsTimer == 3) {
+    stopTimer();
+    $('.loading').css('display', 'none');
+    displayGrid();
+  }
 }
 
 function getTeams(selectObject) {
-  var value = selectObject.value;
+  valueT = selectObject.value;
   $('.errorTeams').css('opacity', '0');
-  console.log(value);
 }
 
 function getMode(selectObject) {
-  var value = selectObject.value;
+  valueM = selectObject.value;
   $('.errorMode').css('opacity', '0');
-  console.log(value);
+}
+
+function stopTimer() {
+  clearTimeout(Timer);
+}
+
+function id(id) {
+  return document.getElementById(id);
 }
